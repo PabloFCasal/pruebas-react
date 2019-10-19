a1d2940c6016b582dd663e9f9691c861c770d95d
Pablo Fernández <pablofcasal@gmail.com>
2019-10-19 22:40:22 +0200

    Added and running SimpleCompnent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import SimpleComponent from './components/SimpleComponent';
 
 function App() {
   return (
     <div className="components">
-    
+      <h2>Varios tipos de componentes en <strong>REACT</strong></h2>
+
+      <h3>Componente simple</h3>
+      <SimpleComponent/>
   
     </div>
   );
---
+++ src/components/SimpleComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class SimpleComponent extends Component {
+  render() {
+    return(
+      <div>
+        <h5>Just a simple Component</h5>
+      </div>
+    )
+  }
+
+
+}
