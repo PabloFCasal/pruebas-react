0fb8c95ff5a53006b535ea023909e74bf836387c
Pablo Fernández <pablofcasal@gmail.com>
2019-10-19 23:32:24 +0200

    Added a component created from a function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React from 'react';
 import './App.css';
 import SimpleComponent from './components/SimpleComponent';
 
-function App() {
-  return (
-    <div className="components">
-      <h2>Varios tipos de componentes en <strong>REACT</strong></h2>
 
-      <h3>Componente simple</h3>
-      <SimpleComponent/>
+
+function ComponentAsAFunction (props){
+
+    return(
+      <p>I am a simple component too</p>
+    )
+}
+
+
+
+
+function App() {
+
+ 
+
+    return (
+      <div className="components">
+        <h2>Varios tipos de componentes en <strong>REACT</strong></h2>
   
-    </div>
-  );
+        <h3>Simple Component</h3>
+        <SimpleComponent/>
+  
+        <h3>Component as a function</h3>
+        <ComponentAsAFunction/>
+  
+      </div>
+    );
+
+
+  
+
+
+  
 }
 
 export default App;
---
+++ src/components/ComponentWithProp.js
@@ -0,0 +1,23 @@
+import React,  {Component} from 'react';
+
+
+export default class ComponentWithProp extends Component {
+
+    constructor (
+        
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
